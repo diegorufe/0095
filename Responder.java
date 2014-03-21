9c4a8bfc8a8287c89acdfdd6460957800b49a24e
Diego <diegorufe@gmail.com>
2014-03-21 13:04:58 +0100

    Anadidos atributos Random y ArrayList e inicializados en el constructor y modificado metodo generateresponse() para que genere respuestas al azar

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,25 @@
  */
 public class Responder
 {
+    Random aleatorio;
+    ArrayList<String> respuestas;
+
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        respuestas = new ArrayList<String>();
+        String[] respuestasDescritas = new String[5];
+        respuestasDescritas[0] = "Describa su problema"; 
+        respuestasDescritas[1] = "¿Esta seguro/a de que ese es su problema?";
+        respuestasDescritas[2] = "Describame el problema de nuevo, por favor";
+        respuestasDescritas[3]= "Interesante, describa mas detalles de su problema";
+        respuestasDescritas[4] = "¿Esta seguro/a de que de verdad tiene un problema?";
+        for(int i =0;i <5;i++){
+            respuestas.add(respuestasDescritas[i]);
+        }
     }
 
     /**
@@ -20,6 +36,7 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return respuestas.get(aleatorio.nextInt(respuestas.size()));
     }
+
 }
